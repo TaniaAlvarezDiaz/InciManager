4b3af4c5ffba5d3925fb179d85ce79e1481438fe
TaniaAlvarezDiaz <tani.alvarez.diaz@gmail.com>
2018-06-21 13:34:56 +0200

    borrado metodo e imports innecesarios

--- src/main/java/controllers/HomeController.java
+++ src/main/java/controllers/HomeController.java
@@ -1,21 +1,12 @@
 package controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import model.Agent;
-import services.AgentService;
-import util.ReaderCSV;
 
 @Controller
 public class HomeController {
-
-	@Autowired
-	private AgentService agentService;
 	
 	@RequestMapping("/")
 	public String mostrarLogin(Model model) {
--- src/main/java/services/AgentService.java
+++ src/main/java/services/AgentService.java
@@ -1,18 +1,13 @@
 package services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import model.Agent;
 import repositories.AgentRepository;
 
 @Service
-public class AgentService implements GetAgent {//, UserDetailsService {
+public class AgentService implements GetAgent {
 
 	@Autowired
 	private AgentRepository agentRepository;
@@ -26,15 +21,5 @@ public class AgentService implements GetAgent {//, UserDetailsService {
 	public Agent find(String identificador, String password, String tipo) {
 		return agentRepository.find(identificador, password, tipo);
 	}
-
-//	@Override
-//	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//		Agent agent = findByIdentificador(username);
-//		
-//		//https://docs.spring.io/spring-security/site/docs/4.2.6.RELEASE/apidocs/org/springframework/security/core/userdetails/User.html
-//		//https://docs.spring.io/spring-security/site/docs/4.2.6.RELEASE/apidocs/org/springframework/security/core/authority/SimpleGrantedAuthority.html
-//
-//		 return new User(username, agent.getPassword(), new SimpleGrantedAuthority("ROLE_AGENT"));
-//	}
 
 }
