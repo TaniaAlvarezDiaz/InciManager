f78ff8a975595fb31ad4c921e3fafdded65e366b
Tania Alvarez Diaz <35775617+TaniaAlvarezDiaz@users.noreply.github.com>
2018-06-23 00:14:08 +0200

    Update WebSecurityConfig.java

--- src/main/java/util/WebSecurityConfig.java
+++ src/main/java/util/WebSecurityConfig.java
@@ -45,10 +45,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-//		http.csrf().disable().authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/login")
-//				.defaultSuccessUrl("/home").loginProcessingUrl("/login").usernameParameter("username")
-//				.passwordParameter("password").permitAll().and().logout().permitAll(false);
-		
 		http.csrf().disable().authorizeRequests()
 		 .antMatchers("/css/**", "/img/**", "/script/**", "/", "/signup",
 		 "/login/**").permitAll().anyRequest()
